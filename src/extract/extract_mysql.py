d616d2b046b4e66e8b94dad8840398743e997419
Vijay Naidu <naiduvijay1000@gmail.com>
2026-02-18 20:39:40 +0530

    injecting nominee and understanding array insert

--- src/extract/extract_mysql.py
+++ src/extract/extract_mysql.py
@@ -20,13 +20,16 @@ def get_connection():
 def extract_data():
    mycursor = get_connection()
    cursor = mycursor.cursor(dictionary=True)
-   cursor.execute("SELECT * FROM pmo02000")
+   cursor.execute("SELECT * FROM pmo02000 po left join pmc03000 p3 " \
+   "on po.policy_number = p3.policy_number and po.owning_location = p3.owning_location " \
+   "left join pmc04000 p4 on po.policy_number = p4.policy_number and po.owning_location = p4.owning_location")
    data = cursor.fetchall()
    mycursor.close()
+  #  print(data)
    return data
 
 if __name__ == "__main__":
     data = extract_data()
     transformed_data = transform(data)
     load_mongo(transformed_data)
-    # print(data)
+#     # print(data)
