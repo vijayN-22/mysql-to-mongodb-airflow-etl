8d1cc2f4546f39ef3c710417e68919bff5ebb45a
Vijay Naidu <naiduvijay1000@gmail.com>
2026-03-27 03:01:24 +0530

    airflow in venv

--- airflow/dags/airflow_sparkdag.py
+++ airflow/dags/airflow_sparkdag.py
@@ -2,19 +2,29 @@ from airflow import DAG
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from datetime import datetime
 
+SPARK_SCRIPT = "/Users/rahul/Documents/python/Data_Engineer/src/spark/spark_transform.py"
+
 with DAG(
-    dag_id="sparkdag",
-    start_date=datetime(2026,3,1),
-    schedule="@daily",   # ✅ correct for Airflow 3
+    dag_id="policy_migration_pipeline",
+    start_date=datetime(2026,3,21),
+    schedule="@daily",
     catchup=False,
     tags=["pyspark","mysql","mongodb"]
 ) as dag:
 
-    spark_job = SparkSubmitOperator(
+    run_policy_pipeline = SparkSubmitOperator(
         task_id="run_policy_migration",
-        application="/Users/rahul/Documents/python/Data_Engineer/src/spark/spark_transform.py",
+        application=SPARK_SCRIPT,
+        packages="org.mongodb.spark:mongo-spark-connector_2.13:10.3.0,mysql:mysql-connector-java:8.0.33",
         conn_id="spark_default",
+        env_vars={
+        "PYTHONPATH": "/Users/rahul/Documents/python/Data_Engineer"
+    },
+        conf={
+        "spark.driverEnv.PYTHONPATH": "/Users/rahul/Documents/python/Data_Engineer",
+        "spark.executorEnv.PYTHONPATH": "/Users/rahul/Documents/python/Data_Engineer"
+    },
         verbose=True
     )
 
-    spark_job
+    run_policy_pipeline
